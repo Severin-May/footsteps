3554047ff5928f05c32411fbf1c24986dd5d4602
minihorse5 <rimr52@inf.elte.hu>
2025-05-11 15:49:41 +0200

    added requirements.txt

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from mqtt_listener import mqtt_listen
 def main():
     print("Hello, this is the main function!")
     db_init()
-    mqtt_listen
+    mqtt_listen()
     print("Hello, this is the end of the main function!")
 
 if __name__ == "__main__":
